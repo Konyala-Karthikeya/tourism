4bc9b36a56b722d552e5e6ae46aa1e5d2c166e9c
19-481 galipalli Supriya <supriyagalipalli@gmail.com>
2024-02-08 15:09:47 +0530

    added header and images

--- Frontend/Tourism/src/app/app.module.ts
+++ Frontend/Tourism/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PackagesComponent } from './packages/packages.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PackagesComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
