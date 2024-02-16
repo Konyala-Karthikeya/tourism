2b18f437af750969a41618561efaf19464e1f871
Karthikeya Konyala <konyalakarthikeya29@gmail.com>
2024-02-16 09:36:46 +0530

    Modified the routing

--- Frontend/Tourism/src/app/app-routing.module.ts
+++ Frontend/Tourism/src/app/app-routing.module.ts
@@ -11,12 +11,9 @@ import { LogoutComponent } from './logout/logout.component';
 import { PackageInfoComponent } from './package-info/package-info.component';
 import { OtpComponent } from './otp/otp.component';
 import { MybookingsComponent } from './mybookings/mybookings.component';
-<<<<<<< HEAD
 import { AdminComponent } from './admin/admin.component';
 import { CustomersComponent } from './customers/customers.component';
-=======
 import { PaymentComponent } from './payment/payment.component';
->>>>>>> 0500d62cd6c62e3146f3b350e7f8e944f5abb95f
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home page if no path provided
@@ -27,17 +24,12 @@ const routes: Routes = [
   { path: 'packages', canActivate: [authGuard], component: PackagesComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'package/:id', component: PackageInfoComponent }, // Route for package info with ID parameter
-<<<<<<< HEAD
   { path: 'otp',        canActivate:[authGuard],   component:OtpComponent },
-  { path: 'mybookings', component: MybookingsComponent },
   { path: 'admin', canActivate:[authGuard], component:AdminComponent},
-  { path: 'customers', canActivate:[authGuard], component:CustomersComponent}
-=======
+  { path: 'customers', canActivate:[authGuard], component:CustomersComponent},
   { path: 'otp', canActivate: [authGuard], component: OtpComponent },
   { path: 'mybookings', component: MybookingsComponent }, // Route for My Bookings component
-  { path: 'payment', component: PaymentComponent }, // Route for My Bookings component
-
->>>>>>> 0500d62cd6c62e3146f3b350e7f8e944f5abb95f
+  { path: 'payment', component: PaymentComponent } // Route for My Bookings component
 ];
 
 @NgModule({
