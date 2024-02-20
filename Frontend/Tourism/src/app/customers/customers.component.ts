958449481fac4a2000f6cc0f4f72a33121d8adcd
Karthikeya Konyala <konyalakarthikeya29@gmail.com>
2024-02-21 00:02:45 +0530

    modified the some components and UI

--- Frontend/Tourism/src/app/customers/customers.component.ts
+++ Frontend/Tourism/src/app/customers/customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../customer.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-customers',
@@ -10,10 +11,22 @@ export class CustomersComponent implements OnInit{
 
   customers : any;
 
-  constructor(private service : CustomerService){ }
+  constructor(private service : CustomerService, private toastr: ToastrService){ }
 
   ngOnInit(){
     this.service.getAllCustomers().subscribe((data: any) => { this.customers = data; });
+  }
+
+  deleteEmployee(customers: any) {
+    this.service.deleteEmployee(customers.customerId).subscribe((data: any) => {console.log(data);});
+
+    const i = this.customers.findIndex((element: any) => {
+      return element.customerId == customers.customerId;
+    });
+
+    this.customers.splice(i, 1);
+
+    this.toastr.success('Employee Deleted Successfully!!!');
   }
 
 }
