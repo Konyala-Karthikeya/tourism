7c6b2512e7a66d3b28efe113cbe8ff66e89e25e8
19-481 galipalli Supriya <supriyagalipalli@gmail.com>
2024-02-21 17:48:24 +0530

    modified few changes

--- Frontend/Tourism/src/app/header1/header1.component.ts
+++ Frontend/Tourism/src/app/header1/header1.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header1',
   templateUrl: './header1.component.html',
   styleUrls: ['./header1.component.css']
 })
-export class Header1Component  {
+export class Header1Component {
 
-  
-  
+  constructor(private router: Router) { }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
